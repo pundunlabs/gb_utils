99ea4cfcde47aa5089d2286b8b0d509fb938bd12
jonasf <jonas.falkevik@mobilearts.com>
2017-04-18 08:58:58 +0200

    First commit of gb_utils

---
+++ src/gb_beam_autoloader.erl
@@ -0,0 +1,159 @@
+%%%-------------------------------------------------------------------
+%%% @author jonas
+%%% @copyright (C) 2017, jonas
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2017-04-13 11:47:33.226508
+%%%-------------------------------------------------------------------
+-module(gb_beam_autoloader).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+-export([start/0,
+	 stop/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+
+-include_lib("gb_log/include/gb_log.hrl").
+-define(INTERVAL, 5*1000).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+start() ->
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:stop(?MODULE).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    erlang:send_after(?INTERVAL, self(), check),
+    {ok, #{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(check, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(check, State) ->
+    All = code:all_loaded(),
+    R = [check_md5(B) || B <- All],
+    print_loaded(lists:flatten(R)),
+    erlang:send_after(?INTERVAL, self(), check),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+        {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+check_md5({M, Path}) ->
+    try
+	{ok, {M, FileMD5}} = beam_lib:md5(Path),
+	CodeMd5 = M:module_info(md5),
+	load_if_diff(M, FileMD5, CodeMd5, Path)
+    catch _:_ ->
+	[]
+    end.
+	    
+
+load_if_diff(_M, Md5, Md5, _Path) ->
+    [];
+load_if_diff(M, _, _, _Path) ->
+    code:purge(M),
+    code:load_file(M),
+    M.
+
+print_loaded([]) ->
+    ok;
+print_loaded(L) ->
+    ?debug("loaded beams: ~p", [L]),
+    io:format("loaded beams: ~p~n", [L]).
